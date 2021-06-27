43724d2a44eb236d731be6c8a10a0d974fbab43f
Ethan Prater Fahey <ecpraterfahey@gmail.com>
2021-06-27 15:44:03 -0700

    feat: finish latent growth curve model definition and demo

--- R/latent-growth-curve/latent_growth_curve_model.r
+++ R/latent-growth-curve/latent_growth_curve_model.r
@@ -1,10 +1,26 @@
 library(here)
-source(here("../sem-parts.r"))
+source(here("R/sem-parts.r"))
+source(here("R/latent-growth-curve/level-factor.r"))
+source(here("R/latent-growth-curve/slopes.r"))
+source(here("R/latent-growth-curve/means.r"))
+source(here("R/latent-growth-curve/variances.r"))
 
-define_latent_growth_curve_model = function(df, model_strength = "CONFIGURAL", unconstrained_parcel_indices = c()){
+define_latent_growth_curve_model = function(df, variable_of_interest_name, model_type, model_strength = "CONFIGURAL", unconstrained_parcel_indices = c(), slope_weights=c()){
+  slope_weights_provided = length(slope_weights) > 0
+  slope_weights_to_pass = if(slope_weights_provided) {
+    slope_weights
+  } else {
+    base=c(0)
+    tail = sapply(2:length(df[,1]), function(e) {NA})
+    append(base,tail)
+    base
+  }
+
   is_weak <- model_strength == "WEAK"
   is_strong <- model_strength == "STRONG" || model_strength == "PARTIAL_STRONG"
   is_strict <- model_strength == "STRICT"
+
+  include_slope = model_type == "LINEAR" || model_type == "LATENT_BASIS"
   paste0(
     "# Define factors\n\n",
     define_factors(df, is_weak || is_strong || is_strict),
@@ -19,6 +35,14 @@ define_latent_growth_curve_model = function(df, model_strength = "CONFIGURAL", u
     "\n\n# Latent variable variances and covariances\n",
     define_latent_variable_variances(df),
     "\n\n",
-    define_latent_variable_covariances(df, TRUE)
+    define_latent_variable_covariances(df, TRUE),
+    "\n\n# Level factor loadings\n",
+    define_level_factor(df, variable_of_interest_name),
+    if(include_slope) "\n\n# Slope factor loadings\n" else "",
+    if(include_slope) define_slope_factor(df, variable_of_interest_name, slope_weights_to_pass) else "",
+    "\n\n# Means\n",
+    define_means(variable_of_interest_name, include_slope),
+    "\n\n# Variances\n",
+    define_level_slope_variances(variable_of_interest_name, include_slope)
   )
 }
