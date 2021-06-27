6948e81f4ea3aa3cf8dcdc4d6cce67da32d5da29
Ethan Prater Fahey <ecpraterfahey@gmail.com>
2021-06-27 15:10:01 -0700

    test: for when there are na in the slope weights

--- tests/testthat/latent-growth-curve/test-slopes.r
+++ tests/testthat/latent-growth-curve/test-slopes.r
@@ -10,3 +10,15 @@ test_that("define_slope_factor",{
   result <- define_slope_factor(dat_new, 'bilbo', slope_weights)
   expect_equal(result, 'bilbo_slope =~ 0*factor1 + 3*factor2')
 })
+
+test_that("define_slope_factor - given NAs",{
+  dat_new <- data.frame(
+    factor1=c("var1","var2"),
+    factor2=c("2var1","2var2"),
+    factor3=c("2var1","2var2"),
+    factor4=c("2var1","2var2")
+  )
+  slope_weights = c(0,NA,NA,5)
+  result <- define_slope_factor(dat_new, 'bilbo', slope_weights)
+  expect_equal(result, 'bilbo_slope =~ 0*factor1 + NA*factor2 + NA*factor3 + 5*factor4')
+})
