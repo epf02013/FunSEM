1a320eca31e5cadf14c010df0104a708e12d16f8
Ethan Prater Fahey <ecpraterfahey@gmail.com>
2021-06-27 15:13:36 -0700

    fix: variances should use double tilde

--- tests/testthat/latent-growth-curve/test-variances.r
+++ tests/testthat/latent-growth-curve/test-variances.r
@@ -3,16 +3,16 @@ source(here("R/latent-growth-curve/variances.r"))
 
 test_that("define_level_slope_variances",{
   result <- define_level_slope_variances("variable_of_interest_name")
-  expect_equal(result, 'variable_of_interest_name_level ~ variable_of_interest_name_level')
+  expect_equal(result, 'variable_of_interest_name_level ~~ variable_of_interest_name_level')
 })
 
 
 test_that("define_level_slope_variances - with include_slopes true",{
   result <- define_level_slope_variances("pop", TRUE)
   pasteResult <- paste(
-    'pop_level ~ pop_level',
-    'pop_level ~ pop_slope',
-    'pop_slope ~ pop_slope',
+    'pop_level ~~ pop_level',
+    'pop_level ~~ pop_slope',
+    'pop_slope ~~ pop_slope',
     sep = "\n"
   )
   expect_equal(result, pasteResult)
