ae2bbfcbdcc5414b7ee1532031f22adc9330547c
Ethan Prater Fahey <ecpraterfahey@gmail.com>
2021-06-27 15:17:54 -0700

    feat: pass variable name to define level factor

--- R/latent-growth-curve/level-factor.r
+++ R/latent-growth-curve/level-factor.r
@@ -1,7 +1,7 @@
 
-define_level_factor <- function (factor_sets) {
+define_level_factor <- function (factor_sets, variable_of_interest_name) {
   factor_names = colnames(factor_sets)
   added_factors = paste(factor_names, collapse = " + 1*")
-  paste0("level =~ 1*",added_factors)
+  paste0(variable_of_interest_name, "_level =~ 1*",added_factors)
 }
 
--- tests/testthat/latent-growth-curve/test-level-factor.r
+++ tests/testthat/latent-growth-curve/test-level-factor.r
@@ -6,6 +6,6 @@ test_that("define_level_factor",{
       factor1=c("var1","var2"),
       factor2=c("2var1","2var2")
     )
-    result <- define_level_factor(dat_new)
-    expect_equal(result, 'level =~ 1*factor1 + 1*factor2')
+    result <- define_level_factor(dat_new, "pop")
+    expect_equal(result, 'pop_level =~ 1*factor1 + 1*factor2')
 })
