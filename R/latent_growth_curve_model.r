4026edad01f9e8b8a9102ee6bf74c1ffe41b27cf
Ethan Prater Fahey <ecpraterfahey@gmail.com>
2021-06-27 14:15:17 -0700

    feat: start latent growth curve model
      added constraining to zero

---
+++ R/latent_growth_curve_model.r
@@ -0,0 +1,24 @@
+library(here)
+source(here("R/sem-parts.r"))
+
+define_latent_growth_curve_model = function(df, model_strength = "CONFIGURAL", unconstrained_parcel_indices = c()){
+  is_weak <- model_strength == "WEAK"
+  is_strong <- model_strength == "STRONG" || model_strength == "PARTIAL_STRONG"
+  is_strict <- model_strength == "STRICT"
+  paste0(
+    "# Define factors\n\n",
+    define_factors(df, is_weak || is_strong || is_strict),
+    "\n\n# Intercepts\n",
+    define_intercepts(df, is_strong || is_strict, unconstrained_parcel_indices),
+    "\n\n# Unique variances and covariances\n",
+    define_variances(df, is_strict),
+    "\n\n",
+    define_covariances(df),
+    "\n\n# Latent variable means\n",
+    define_latent_variable_means(df),
+    "\n\n# Latent variable variances and covariances\n",
+    define_latent_variable_variances(df),
+    "\n\n",
+    define_latent_variable_covariances(df, TRUE)
+  )
+}
