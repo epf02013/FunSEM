b4dec128b45fc3c9c724949ba25ae96c7941d856
Ethan Prater Fahey <ecpraterfahey@gmail.com>
2021-09-19 10:29:20 -0700

    feat: constrain all means to zero for longitudinal_measurement_invariance_model when model_strength is CONFIGURAL

--- R/longitudinal_measurement_invariance_model.r
+++ R/longitudinal_measurement_invariance_model.r
@@ -3,6 +3,7 @@ define_longitudinal_measurement_invariance_model = function(df, model_strength =
   is_weak <- model_strength == "WEAK"
   is_strong <- model_strength == "STRONG" || model_strength == "PARTIAL_STRONG"
   is_strict <- model_strength == "STRICT"
+  constrain_all_means_to_zero <- model_strength == "CONFIGURAL"
   paste0(
     "# Define factors\n\n",
     define_factors(df, is_weak || is_strong || is_strict),
@@ -13,7 +14,7 @@ define_longitudinal_measurement_invariance_model = function(df, model_strength =
     "\n\n",
     define_covariances(df),
     "\n\n# Latent variable means\n",
-    define_latent_variable_means(df, FALSE),
+    define_latent_variable_means(df, constrain_all_means_to_zero),
     "\n\n# Latent variable variances and covariances\n",
     define_latent_variable_variances(df),
     "\n\n",
